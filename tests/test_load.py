655cc135b0030d71153b43755e7a34c4966b686b
Lizzy <lizzywhite018@icloud.com>
2026-09-20 16:48:19 +0100

    additional schema check

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,4 +1,6 @@
 import duckdb
+import pandera.errors
+import pytest
 
 from home_credit_risk.data.schema import application_train_schema
 
@@ -15,3 +17,19 @@ def test_application_train_schema_on_fixture():
     df = con.execute("SELECT * FROM application_train").df()
 
     application_train_schema.validate(df)
+
+
+def test_application_train_schema_rejects_bad_target():
+    """
+    Target is out of range
+    """
+    con = duckdb.connect()
+    con.execute("""
+        CREATE TABLE bad_data AS
+        SELECT * FROM (VALUES (100001, 2, 180000.0))
+        AS t(SK_ID_CURR, TARGET, AMT_INCOME_TOTAL)
+    """)
+    df = con.execute("SELECT * FROM bad_data").df()
+
+    with pytest.raises(pandera.errors.SchemaError):
+        application_train_schema.validate(df)
